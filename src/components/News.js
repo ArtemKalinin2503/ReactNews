f710900c6c25a93996764aad0d744f9fb51e5d3d
kalinin <kalinin@inventive.ru>
2019-02-20 22:05:39 +0300

    three

--- src/components/News.js
+++ src/components/News.js
@@ -18,10 +18,18 @@ class News extends Component {
 
     //Кнопка Сохранить
     handleSaveNews = () => {
+        let inputEditNews = this.refs.newEditNews.value; //С помощью ref (ссылка) получим значение (value) teatarea
+        this.props.updateNews(inputEditNews, this.props.index)
         this.setState({
             edit: !this.state.edit //Вернем противоположное значение состояния edit (если true тогда изменим классы для элементов в новости)
         });
+
     };
+
+    //Кнопка Удалить
+    handleDeleteNews= () => {
+        this.props.deleteNews(this.props.index); //Вызывыем функцию deleteBlock (которую мы передали в атрибут deleteNews)
+    }
 
     render() {
         return (
@@ -29,7 +37,7 @@ class News extends Component {
                 <p>{this.props.description}</p> {/*Данные доступны благодаря переданнуму state из компонента App в атрибутах*/}
                 <button onClick={this.handleEditNews} className={this.state.edit ? 'edit-active': ''}>Редактировать</button>
                 <button onClick={this.handleDeleteNews}  className={this.state.edit ? 'edit-active': ''}>Удалить</button>
-                <textarea className={this.state.edit ? 'active-edit-news': 'no-active-edit-news'} placeholder="Изменить новость"></textarea>
+                <textarea ref="newEditNews" className={this.state.edit ? 'active-edit-news': 'no-active-edit-news'} placeholder="Изменить новость"></textarea>
                 <button onClick={this.handleSaveNews} className={this.state.edit ? 'active-edit-news': 'no-active-edit-news'}>Сохранить</button>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -23,14 +23,36 @@ class App extends Component {
         ]
     }
   };
+
+  //Функция которая будет удалять Новость
+  deleteBlock = (index) => { //index это индекс экземпляра нашего компонента (в данном случае компонента News)
+    let arrNews = this.state.news; //Создадим новый массив в который передадим наш массив объектов news
+    arrNews.splice(index, 1); //С помощью метода splice (первый параметр это идекс элемента, а второй парметр это каличество элементов которые нужно удалить)
+    this.setState({
+      news: arrNews //Передадим в наш state news новый массив arrNews
+    }); 
+  };
+
+  //Функция которая будет изменять Новость
+  updateBlock = (text, index) => {
+    let arrNews = this.state.news;
+    arrNews[index] = text;
+    console.log(arrNews)
+    this.setState({
+      news: arrNews
+    });
+  };
   
   render() {
   	const listNews = this.state.news.map((item, index) => { //С помощью метода map будем выводить компонент News (столько раз сколько записей в самом массиве объектов News)
 			return <News
                 //С помощью атрибутов прокинем данные (state) для доступа к этим данным через props из массива item (в который передали массив объектов News)
-				        key={index}
+				        key={index} //Данный атрибут нужен для React (согласно документации)
+                index={index} //Данный атрибут нужен чтобы мы могли брать конкретный блок (экземпляр нашего компонента)
 				        id={item.id} 
 				        description={item.description}
+                updateNews={this.updateBlock}
+                deleteNews={this.deleteBlock} //В качестве атрибута пердадим функция deleteBlock (чтобы она была доступна компоненту News через props)
 			       />;
 		});
 
